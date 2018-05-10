b7dd1fdbf1b6f1f92fed50c2369f591d68eb1c89
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-05-10 16:20:19 +0200

    example listing and basic examples

--- exemples.py
+++ exemples.py
@@ -1,3 +1,14 @@
+import glob
+import argparse
+import algo
+import logging
+
+def analyze_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="print debug logs, slows the program a lot, shouldn't run on big graph nor many examples", action="store_true")
+    return parser.parse_args()
+
+
 def file_to_graph(filename):
     with open(filename, 'r') as f:
         lines = f.read().splitlines()
@@ -13,3 +24,20 @@ def add_edge_to_graph(graph, i, j):
     if i not in graph:
         graph[i] = []
     graph[i].append(j)
+
+
+def main():
+    args = analyze_args()
+
+    level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(format='[%(levelname)s] %(message)s', level=level)
+    graphs = []
+    logging.info(msg="Parsing examples...")
+    for f in glob.glob("exemples/*"):
+        graphs.append(file_to_graph(f))
+    logging.info(msg="Graph generation finished")
+    algo.run_case_suite_and_export(graphs)
+
+
+if __name__ == '__main__':
+    main()
